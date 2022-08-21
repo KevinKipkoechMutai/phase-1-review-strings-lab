183c9af36cb7eb599247fa2dbedd796535a5281f
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-08-21 05:11:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+//definition of the currentUser variable and assigning it to a string
+const currentUser = "Kevin";
+
+//introducing the welcomeMessage variable
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//introducing the excitedWelcomeMessage variable
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+//introducing the shortGreeting variable
+const firstInitial = currentUser[0];
+let shortGreeting = `Welcome, ${firstInitial}!`;
